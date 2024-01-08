16596cf3a14cb1cd0a7f012d9bb3541a38291675
FORMULA-12 <formula120877@gmail.com>
2024-01-08 17:04:16 -0500

    Discord Bot

---
+++ scripts/discord.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const { Client, GatewayIntentBits } = require('discord.js')
+
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]
+});
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('guildMemberAdd', member => {
+    console.log('User: ' + member.user.username + ' has joined the server!');
+    let role = member.guild.roles.cache.find(role => role.name === "📔 | Участник");
+    member.roles.add(role);
+});
+
+
+dotenv.config();
+
+client.login(process.env.DISCORD_TOKEN);
