e8ee7cf139a6d7fb3f76211bb1df43d7679f0bb5
FORMULA-12 <formula120877@gmail.com>
2024-01-11 16:28:34 -0500

    Rating system fixed

--- scripts/rating.js
+++ scripts/rating.js
@@ -16,77 +16,22 @@ async function checkRating() {
 
         await client.connect();
 
-        const result = await client.query('SELECT * FROM servers;');
+        const result = await client.query(`SELECT * FROM servers WHERE ban = false;`);
         const servers = result.rows;
 
         for (const server of servers) {
-            const serverUrl = `https://api.mcstatus.io/v2/status/java/${server.ip}:${server.port}`;
-
             try {
-                const response = await axios.get(serverUrl);
-                const serverStatus = response.data;
+                let rate = 10;
 
-                const currentPlayers = serverStatus.players.online;
+                if (server.banner != 'default-server-picture.gif') rate = rate + 10;
+                if (server.premium_status) rate = rate + 10;
+                if (server.verification) rate = rate + 10;
+                if (server.current_players >= 5) rate = rate + 10;
+                if (server.current_players >= 40) rate = rate + 10;
+                if (server.current_players >= 100) rate = rate + 10;
 
-                if (server.banner != 'default-server-picture.gif') {
-                    if (!server.banner_rate_add) {
-                        await client.query(`UPDATE servers SET banner_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.banner_rate_add) {
-                        await client.query(`UPDATE servers SET banner_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }
-
-                if (server.premium_status) {
-                    if (!server.premium_rate_add) {
-                        await client.query(`UPDATE servers SET premium_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.premium_rate_add) {
-                        await client.query(`UPDATE servers SET premium_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }
-
-                if (server.verification) {
-                    if (!server.verification_rate_add) {
-                        await client.query(`UPDATE servers SET verification_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.verification_rate_add) {
-                        await client.query(`UPDATE servers SET verification_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }
-
-                if (currentPlayers > 5) {
-                    if (!server.activity_rate_add) {
-                        await client.query(`UPDATE servers SET activity_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.activity_rate_add) {
-                        await client.query(`UPDATE servers SET activity_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }        
-                
-                if (currentPlayers > 40) {
-                    if (!server.popularity_rate_add) {
-                        await client.query(`UPDATE servers SET popularity_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.popularity_rate_add) {
-                        await client.query(`UPDATE servers SET popularity_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }
-
-                if (currentPlayers > 100) {
-                    if (!server.superiority_rate_add) {
-                        await client.query(`UPDATE servers SET superiority_rate_add = true, rate = $1 WHERE ip = $2;`, [(server.rate + 10), server.ip]);
-                    }
-                } else {
-                    if (server.superiority_rate_add) {
-                        await client.query(`UPDATE servers SET superiority_rate_add = false, rate = $1 WHERE ip = $2;`, [(server.rate - 10), server.ip]);
-                    }
-                }
+                console.log(`${server.ip}: ${rate}★`);
+                await client.query(`UPDATE servers SET rate = $1 WHERE ip = $2;`, [rate, server.ip]);
             } catch (error) {
                 console.error(error.message);
             }
